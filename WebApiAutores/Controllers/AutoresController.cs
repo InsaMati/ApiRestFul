13fc64ed98690463d5db0ec340b236b03d1b2980
Matias <you@example.com>
2022-03-09 15:46:38 -0300

    Migration

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { id = 1, Nombre = "Matias"},
+                new Autor() { id = 2, Nombre = "Elian"}
+            };
+        }
+    }
+}
---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
